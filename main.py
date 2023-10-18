a685a00a0f3d86fd26597326c4f984d2fccfb5a5
MATYIKO\Matyiko <matyi.szakszon@gmail.com>
2023-10-18 21:26:27 +0200

    Revert "Revert "remove unnecceserry imports""
    This reverts commit c91b6140f3b5f92ca35e3ccadfc7ab98a4f17454.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 '''Train CIFAR10 with PyTorch.'''
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
 
 import torchvision
 import torchvision.transforms as transforms
 
-import os
 import argparse
 
 
